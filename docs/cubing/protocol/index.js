1b0728f521a70ff8b5049491eefb3f28cfc9eda7
samuelpcarter <samcart37@gmail.com>
2025-06-27 10:48:08 -0700

    Add 4x4 Rubik's Cube simulator with solver

---
+++ docs/cubing/protocol/index.js
@@ -0,0 +1,20 @@
+import {
+  binaryComponentsToReid3x3x3,
+  bufferToSpacedHex,
+  reid3x3x3ToTwizzleBinary,
+  spacedHexToBuffer,
+  twizzleBinaryToBinaryComponents,
+  twizzleBinaryToReid3x3x3
+} from "../chunks/chunk-YFGRXDD4.js";
+import "../chunks/chunk-YQVHA4F6.js";
+import "../chunks/chunk-TQF5J7MH.js";
+import "../chunks/chunk-2SBMIHZV.js";
+export {
+  binaryComponentsToReid3x3x3 as experimentalBinaryComponentsToReid3x3x3,
+  bufferToSpacedHex as experimentalBufferToSpacedHex,
+  reid3x3x3ToTwizzleBinary as experimentalReid3x3x3ToTwizzleBinary,
+  spacedHexToBuffer as experimentalSpacedHexToBuffer,
+  twizzleBinaryToBinaryComponents as experimentalTwizzleBinaryToBinaryComponents,
+  twizzleBinaryToReid3x3x3 as experimentalTwizzleBinaryToReid3x3x3
+};
+//# sourceMappingURL=index.js.map
---
+++ docs/cubing/search/index.d.ts
@@ -0,0 +1,7 @@
+export { a as experimentalSolve2x2x2, e as experimentalSolve3x3x3IgnoringCenters, f as experimentalSolveTwsearch, d as setSearchDebug, c as solveMegaminx, b as solvePyraminx, s as solveSkewb } from '../index-g0IQg962.js';
+import { K as KPattern } from '../PuzzleLoader-DWqO9l1G.js';
+import 'three/src/Three.js';
+
+declare function random333Pattern(): Promise<KPattern>;
+
+export { random333Pattern };
